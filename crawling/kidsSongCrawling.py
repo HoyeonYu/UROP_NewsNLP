dccd4257f72d7e1a480957b1cc9748e221f7e577
ghdus819 <ghdus819@gmail.com>
2021-06-28 19:01:20 +0900

    Add Kids Song Data

---
+++ crawling/kidsSongCrawling.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+driver = webdriver.Chrome('C:\\chromeDriver\\chromedriver.exe')
+
+
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+
+def getKidsSongData():
+    url = 'https://music.bugs.co.kr/album/20095145'
+    driver.get(url)
+    search_period_num = 300
+    href_list = []
+    title_list = []
+    description_list = []
+
+    for songIdx in range(search_period_num):
+        print('song idx: ', songIdx)
+
+        if check_exists_by_xpath('//*[@id="ALBUMTRACK20095145"]/table/tbody/tr[' + (str)(songIdx + 1) + ']/td[3]/a'):
+            href = driver.find_element_by_xpath('//*[@id="ALBUMTRACK20095145"]/table/tbody/tr['
+                                                + (str)(songIdx + 1) + ']/td[3]/a').get_attribute('href')
+            href_list.append(href)
+            print(href)
+    cnt = 1
+
+    for href in href_list:
+        print("cnt: ", cnt)
+        driver.get(href)
+
+        print(href)
+
+        if check_exists_by_xpath('//*[@id="container"]/header/div/h1'):
+            title = driver.find_element_by_xpath('//*[@id="container"]/header/div/h1').text
+            title_list.append(title)
+            print("title: " + title)
+
+            description = driver.find_element_by_xpath('//*[@id="container"]/section[2]/div/div/xmp').text
+            description_list.append(description + " ")
+            print("description: " + description)
+
+        cnt += 1
+        driver.get(url)
+
+    kidsSongDic = {
+        '제목': title_list,
+        '내용': description_list
+    }
+
+    naverNewsDf = pd.DataFrame(kidsSongDic)
+    naverNewsDf.to_csv('kidsSongHot.csv', encoding='utf-8-sig', index=True)
+
+    driver.close()
+
+
+getKidsSongData()
--- crawling/ruliWebCrawling.py
+++ crawling/ruliWebCrawling.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome('C:\\chromeDriver\\chromedriver.exe')
 
+
 def check_exists_by_xpath(xpath):
     try:
         driver.find_element_by_xpath(xpath)
@@ -11,47 +12,50 @@ def check_exists_by_xpath(xpath):
         return False
     return True
 
+
 def getRuliWebData():
-    url = 'https://bbs.ruliweb.com/best/selection?orderby=readcount&range=30d&page='
+    url = ['https://bbs.ruliweb.com/best/selection?orderby=readcount&range=30d&page=',
+           'https://bbs.ruliweb.com/best/selection?orderby=recommend&range=30d&page=',
+           'https://bbs.ruliweb.com/best/selection?orderby=replycount&range=30d&page=']
     search_page_num = 100
     title_list = []
     description_list = []
 
-    for pageIdx in range(search_page_num):
-        print(pageIdx)
-        move_url = url + str(pageIdx + 1)
-        driver.get(move_url)
-        links = driver.find_elements_by_class_name('table_body')
+    for urlIdx in range(len(url)):
+        for pageIdx in range(search_page_num):
+            print(pageIdx)
+            move_url = url[urlIdx] + str(pageIdx + 1)
+            driver.get(move_url)
+            links = driver.find_elements_by_class_name('table_body')
 
+            print("len: ", len(links))
 
-        print("len: ", len(links))
+            href_list = []
+            for linkIdx in range(len(links)):
+                href = driver.find_element_by_xpath('// *[ @ id = "best_body"] / table / tbody / \
+                tr[' + str(linkIdx + 1) + '] / td[2] / a').get_attribute('href')
+                href_list.append(href)
+                print(href)
 
-        href_list = []
-        for linkIdx in range(len(links)):
-            href = driver.find_element_by_xpath('// *[ @ id = "best_body"] / table / tbody / \
-            tr[' + str(linkIdx + 1) + '] / td[2] / a').get_attribute('href')
-            href_list.append(href)
-            print(href)
+            cnt = 1
 
-        cnt = 1
+            for href in href_list:
+                print("cnt: ", cnt)
+                driver.get(href)
 
-        for href in href_list:
-            print("cnt: ", cnt)
-            driver.get(href)
+                print(move_url)
 
-            print(move_url)
+                if check_exists_by_xpath('//*[@id="board_read"]/div/div[2]/div[1]/div[1]/div[1]/h4/span/span[2]'):
+                    title = driver.find_element_by_xpath(
+                        '//*[@id="board_read"]/div/div[2]/div[1]/div[1]/div[1]/h4/span/span[2]').text
+                    title_list.append(title)
+                    print("title: " + title)
 
-            if check_exists_by_xpath('//*[@id="board_read"]/div/div[2]/div[1]/div[1]/div[1]/h4/span/span[2]'):
-                title = driver.find_element_by_xpath('//*[@id="board_read"]/div/div[2]/div[1]/div[1]/div[1]/h4/span/span[2]').text
-                title_list.append(title)
-                print("title: " + title)
+                    description = driver.find_element_by_xpath('//*[@id="board_read"]/div/div[2]/div[2]/div[1]').text
+                    description_list.append(description + " ")
+                    # print("description: " + description)
 
-                description = driver.find_element_by_xpath('//*[@id="board_read"]/div/div[2]/div[2]/div[1]').text
-                description_list.append(description + " ")
-                #print("description: " + description)
-
-            cnt += 1
-
+                cnt += 1
 
     ruliWebDic = {
         '제목': title_list,
