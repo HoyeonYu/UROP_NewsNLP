e07b8862f6e67953300ec26b8a6ff9fe04029083
ghdus819 <ghdus819@gmail.com>
2021-07-16 02:32:01 +0900

    Increase News Data

--- crawling/naverNewsCrawling.py
+++ crawling/naverNewsCrawling.py
@@ -14,9 +14,10 @@ def check_exists_by_xpath(xpath):
 
 
 def getNaverNewsData():
-    url = 'https://news.naver.com/main/ranking/popularDay.nhn?date='
-    search_period_num = 100
-    start_period = 20210625
+    url = 'https://news.naver.com/main/ranking/popularDay.naver?date='
+
+    search_period_num = 250
+    start_period = 20210711
     find_period = start_period
     title_list = []
     description_list = []
@@ -31,30 +32,31 @@ def getNaverNewsData():
                 if check_exists_by_xpath('// *[ @ id = "wrap"] / div[4] / div[2] / div / \
                     div[' + str(box_idx + 1) + '] / ul / li[' + str(innerBox_idx + 1) + '] / div / a'):
                     href = driver.find_element_by_xpath('// *[ @ id = "wrap"] / div[4] / div[2] / div / \
-                    div[' + str(box_idx + 1) + '] / ul / li[' + str(innerBox_idx + 1) + '] / div / a').get_attribute('href')
+                    div[' + str(box_idx + 1) + '] / ul / li[' + str(innerBox_idx + 1) + '] / div / a').get_attribute(
+                        'href')
                     href_list.append(href)
-                    print('box_idx: ' + str(box_idx) + ', innerBox_idx: ' + str(innerBox_idx))
+                    # print('box_idx: ' + str(box_idx) + ', innerBox_idx: ' + str(innerBox_idx))
 
         cnt = 1
 
         for href in href_list:
-            print("cnt: ", cnt)
+            # print("cnt: ", cnt)
             driver.get(href)
 
-            print(move_url)
+            # print(move_url)
 
             if check_exists_by_xpath('//*[@id="articleTitle"]'):
                 title = driver.find_element_by_xpath('//*[@id="articleTitle"]').text
                 title_list.append(title)
-                print("title: " + title)
+                print("idx: ", len(title_list), ", date: ", find_period, ". title: " + title)
 
                 description = driver.find_element_by_xpath('//*[@id="articleBodyContents"]').text
                 description_list.append(description + " ")
-                #print("description: " + description)
+                # print("description: " + description)
 
             cnt += 1
 
-        find_period -= 2
+        find_period -= 1
 
         if find_period % 100 == 0 or find_period % 100 > 28:
             find_period -= 72
